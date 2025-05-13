4513e5a1d168b8462f0d0dcc8841a1f41d9751ee
Joevita27 <247007111002@student.unsil.ac.id>
2025-05-13 09:21:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Beranda</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <style>
   .btn-underline {
@@ -238,8 +240,11 @@
 </div>
 
 
-<!-- Bootstrap JS -->
+<!-- Bootstrap Bundle JS CDN -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
 </body>
 </html>
 <?php include 'footer.php'; ?>
